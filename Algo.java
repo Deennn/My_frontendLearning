5441cabe7dc12a7dee69d25329d38cbbf8640737
User <user@Users-MacBook-Air.local>
2022-03-05 13:32:05 +0100

    new commit

---
+++ Algo.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Algo {
+    public static void main(String[] args) {
+        for (int i = 0; i < 11; i++) {
+            System.out.println(i);
+        }
+        int[] arr = new int[4];
+        System.out.println(binarySearch(arr));
+    }
+    public static int binarySearch(int[] arr) {
+        List<Integer> ar = new ArrayList<>();
+        return 1;
+    }
+}
